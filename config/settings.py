3fecf128bbd6fceb33142723a61d70d2df317445
Ivanna Parfeniuk <1998ivankaa@gmail.com>
2021-08-13 20:10:21 +0300

    update Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -26,8 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',    
-    'blog.apps.BlogConfig',
-    'accounts.apps.AccountsConfig',
+    
 ]
 
 MIDDLEWARE = [
