0ffb0c88f1171cbe8be967060daac68a6a09a9de
Ivanna Parfeniuk <1998ivankaa@gmail.com>
2021-08-13 19:35:35 +0300

    forms and user accounts

--- blog/tests.py
+++ blog/tests.py
@@ -15,7 +15,7 @@ class BlogTest(TestCase):
         )
 
         self.post = Post.objects.create(
-            titlle='A good title',
+            title='A good title',
             body='Nice body content',
             author=self.user,
         )
@@ -26,6 +26,10 @@ class BlogTest(TestCase):
         self.assertEqual(str(post), post.title)
 
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
         self.assertEqual(f'{self.post.author}', 'testuser')
@@ -33,16 +37,40 @@ class BlogTest(TestCase):
 
 
     def test_post_list_view(self):
-        reaponse = self.client.get(reverse('home'))
-        self.assertEqual(reaponse.status_code, 200)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Nice body content')
-        self.assertTemplateUsed(reaponse, 'home.html')
+        self.assertTemplateUsed(response, 'home.html')
 
 
     def test_post_detail_view(self):
-        responce = self.client.get('/post/1/')
-        no_response = self.client.get('/post/100000')
-        self.assertEqual(response.staus_code, 200)
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Update text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -109,3 +110,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
